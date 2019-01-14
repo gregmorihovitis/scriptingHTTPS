ce651807cde32825fc0a4f7b3bbdbeffbc75d62c
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-14 19:36:31 +0000

    completed part 4

---
+++ httpScripting4.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+function getHTML (options, callback) {
+let chunks = '';
+
+  https.get(options, function(response){
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      callback(data);
+    });
+
+  //   response.on('end', callback(chunks));
+  // });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
